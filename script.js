08cd55012bd3b0dc1b180271e999c1443f975843
AshiqurRahmanAshik <ashiqurrahmanashik111@gmail.com>
2025-09-06 02:01:12 +0600

    Practice Dynamic Active Status

--- script.js
+++ script.js
@@ -3,12 +3,23 @@ const categoryContainer = document.getElementById("categoryContainer");
 const loadCategory = () => {
   fetch("https://news-api-fs.vercel.app/api/categories")
     .then((res) => res.json())
-    .then((data) => showCategories(data.categories));
+    .then((data) => {
+      displayCategory(data.categories);
+    });
 };
-const showCategories = (categories) => {
+const displayCategory = (categories) => {
+  categoryContainer.addEventListener("click", (e) => {
+    const allLi = categoryContainer.querySelectorAll("li");
+    allLi.forEach((li) => {
+      li.classList.remove("border-b-2", "border-red-600");
+    });
+    if (e.target.localName === "li") {
+      e.target.classList.add("border-b-2", "border-red-600");
+    }
+  });
   categories.forEach((category) => {
     categoryContainer.innerHTML += `
-    <li>${category.title}</li>
+      <li class="hover:border-b-2 border-red-600">${category.title}</li>
     `;
   });
 };
